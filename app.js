76a407bc0e2ded2ccdf4bc7094851a780aa5bb28
nath-asha <omthasha@gmail.com>
2024-02-18 01:33:25 +0530

    x

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
 // Middlewares
-app.use(json()); // Built-in middleware for parsing JSON requests
-
+app.use(express.json()); // Built-in middleware for parsing JSON requests
 
 // Application-level middleware
-import authMiddleware from './middleware/authMiddleware';
+const authMiddleware = require('./middleware/authMiddleware');
 app.use(authMiddleware);
 
 // Router-level middleware
-import authRoutes from './routes/authRoutes';
-import blogRoutes from './routes/blogRoutes';
+const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
 app.use('/auth', authRoutes);
 app.use('/blogs', blogRoutes);
 
